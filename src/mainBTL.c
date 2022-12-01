7a3fb07ee40c8edb86cda26b4b1890178371180b
Magdy Adel <magdyadel608@gmail.com>
2022-12-01 08:34:08 +0200

    update mainBTL

--- src/mainBTL.c
+++ src/mainBTL.c
@@ -1,27 +1,9 @@
-
-#include "STD_TYPES.h"
-#include "BIT_MATH.h"
-#include "FPEC_interface.h"
-
-
-#define state(_statFUN_) 	BTL_enu##_statFUN_State()
-
-void BTL_vStart(void);
-void BTL_vStart(void)
-{
-	while(1)
-	{
-		switch(currentBTLState)
-		{
-			case Waiting : currentBTLState = state(Waiting);  break;
-			case Burning : currentBTLState = state(Burning);  break;
-			case Finished: currentBTLState = state(Finished); break;
-		}
-	}
-}
+#include "../ARM_COTS/LIB/STD_TYPES.h"
+#include "../ARM_COTS/LIB/BIT_MATH.h"
+#include "../APP/BOOTLOADER/BOOTLOADER.h"
 
 
-int mainBTL(void);
+int mainBTL(void)
 {
 	BTL_vSetup();
 	BTL_vStart();
